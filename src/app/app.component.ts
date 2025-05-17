19777e5fe5dfe6614ef7fc1161fe63313a93f822
msRashid276 <rashidmohammed276@gmail.com>
2025-05-17 22:47:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="images/logo2.jpg" alt="logo"  />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+        <app-housing-location></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
