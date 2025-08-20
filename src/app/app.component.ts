273d298ce811269b2b995f4cc4b24a4b9d10feaf
msRashid276 <rashidmohammed276@gmail.com>
2025-08-20 19:54:19 +0530

    details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { HomeComponent } from './home/home.component';
     <main>
       <header class="brand-name" style="display: flex; flex-direction: row;">
         <img class="brand-logo" src="images/logo2.jpg" alt="logo" aria-hidden="true"/>
-        <h1>Hostels</h1>
+        <h1>Homes</h1>
       </header>
       <section class="content">
         <router-outlet></router-outlet>
